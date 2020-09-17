dc9a97b6f86e72c5b8c214f9f0035c479e90e1fa
sashitaghimire <ghimiresashita@gmail.com>
2020-09-17 18:44:31 +0545

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Sidebar.css';
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* twitter icons */}
+            
+
+
+            {/* Siderbar option component */}
+        </div>
+    )
+}
+
+export default Sidebar;
